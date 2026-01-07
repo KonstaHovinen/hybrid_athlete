522499807e3a44e712f61cae3c713ffc643f5a85
KonstaHovinen <KonstaHovinen@users.noreply.github.com>
2026-01-07 08:30:47 +0000

    deploy: d966d2991a8adc4121a6ad9829740458ee9ab21e

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "951480546"
+    serviceWorkerVersion: "892651166"
   }
 });
